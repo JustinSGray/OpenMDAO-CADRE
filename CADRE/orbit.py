f26d2bcb6322a739d1bbbd90234b38a33e56133f
cnkavanaugh <cnkavanaugh@gmail.com>
2013-07-02 12:45:30 -0400

    Update orbit.py

--- CADRE/orbit.py
+++ CADRE/orbit.py
@@ -10,7 +10,7 @@ class Orbit_Initial(Component):
 	argPerigee = Float(0, iotype="in", copy=None)
 	trueAnomaly = Float(0, iotype="in", copy=None)
 	
-    def __init__(self):
+	def __init__(self):
 		super(Orbit_Initial, self).__init__()
 		self.add('r_e2b_I0', Array(np.ones(3)), size=3, dtype=np.float, iotype='out')
 		self.add('v_e2b_I0', Array(np.ones(3)), , size=3, dtype=np.float,iotype='out')
@@ -30,7 +30,7 @@ class Orbit_Initial(Component):
 			R = np.eye(3,dtype=complex) + S(axis)*np.sin(angle) + (1 - np.cos(angle)) * (np.outer(axis,axis) - np.eye(3,dtype=complex))
 			return R
 			
-		d2r = np.pi/180.0
+	d2r = np.pi/180.0
         r_perigee = Re + altPerigee
         r_apogee = Re + altApogee
         e = (r_apogee-r_perigee)/(r_apogee+r_perigee)
@@ -57,36 +57,35 @@ class Orbit_Initial(Component):
 		ih = complex(0,h)
 		v = np.zeros(6,complex)
 		v[:] = [self.altPerigee[0]. self.altApogee[0], slef.RAAN[0], self.Inc[0], self.argPerigee[0], self.trueAnomaly[0])
-		
 		self.J = np.zeros((6,6))
 		for i in range(6):
-            v[i] += ih
-            r0_ECI, v0_ECI = self.compute(v[0], v[1], v[2], v[3], v[4], v[5])
-            v[i] -= ih
-            self.J[:3,i] = r0_ECI.imag/h
-            self.J[3:,i] = v0_ECI.imag/h
+			v[i] += ih
+			r0_ECI, v0_ECI = self.compute(v[0], v[1], v[2], v[3], v[4], v[5])
+			v[i] -= ih
+			self.J[:3,i] = r0_ECI.imag/h
+			self.J[3:,i] = v0_ECI.imag/h
 	
 	def execute(self):
-        r0_ECI, v0_ECI = self.compute(self.altPerigee[0], self.altApogee[0], self.RAAN[0], self.Inc[0], self.argPerigee[0], self.trueAnomaly[0])
-        self.r_e2b_I0 = r0_ECI.real
-        self.v_e2b_I0 = v0_ECI.real
+		r0_ECI, v0_ECI = self.compute(self.altPerigee[0], self.altApogee[0], self.RAAN[0], self.Inc[0], self.argPerigee[0], self.trueAnomaly[0])
+		self.r_e2b_I0 = r0_ECI.real
+		self.v_e2b_I0 = v0_ECI.real
 		
 	def applyDer(salf, arg, result):
 		if not result['r_e2b_I0']:
-            result['r_e2b_I0'] = np.zeros(3)
+			result['r_e2b_I0'] = np.zeros(3)
 		if not result['v_e2b_I0']:
-            result['v_e2b_I0'] = np.zeros(3)
+			result['v_e2b_I0'] = np.zeros(3)
 		J = self.J
-        result['r_e2b_I0'] = J[:3,0]*arg['altPerigee'][0] + J[:3,1]*arg['altApogee'][0] + J[:3,2]*arg['RAAN'][0] + J[:3,3]*arg['Inc'][0] + J[:3,4]*arg['argPerigee'][0] + J[:3,5]*arg['trueAnomaly'][0]
-        result['v_e2b_I0'] = J[3:,0]*arg['altPerigee'][0] + J[3:,1]*arg['altApogee'][0] + J[3:,2]*arg['RAAN'][0] + J[3:,3]*arg['Inc'][0] + J[3:,4]*arg['argPerigee'][0] + J[3:,5]*arg['trueAnomaly'][0]
+		result['r_e2b_I0'] = J[:3,0]*arg['altPerigee'][0] + J[:3,1]*arg['altApogee'][0] + J[:3,2]*arg['RAAN'][0] + J[:3,3]*arg['Inc'][0] + J[:3,4]*arg['argPerigee'][0] + J[:3,5]*arg['trueAnomaly'][0]
+		result['v_e2b_I0'] = J[3:,0]*arg['altPerigee'][0] + J[3:,1]*arg['altApogee'][0] + J[3:,2]*arg['RAAN'][0] + J[3:,3]*arg['Inc'][0] + J[3:,4]*arg['argPerigee'][0] + J[3:,5]*arg['trueAnomaly'][0]
 	
 		return result
 		
-    def applyDerT(self, arg, result):
-        J = self.J
-        result['altPerigee'][0] += sum(J[:3,0]*arg['r_e2b_I0'][:]) + sum(J[3:,0]*arg['v_e2b_I0'][:])
-        result['altApogee'][0] += sum(J[:3,1]*arg['r_e2b_I0'][:]) + sum(J[3:,1]*arg['v_e2b_I0'][:])
-        result['RAAN'][0] += sum(J[:3,2]*arg['r_e2b_I0'][:]) + sum(J[3:,2]*arg['v_e2b_I0'][:])
-        result['Inc'][0] += sum(J[:3,3]*arg['r_e2b_I0'][:]) + sum(J[3:,3]*arg['v_e2b_I0'][:])
-        result['argPerigee'][0] += sum(J[:3,4]*arg['r_e2b_I0'][:]) + sum(J[3:,4]*arg['v_e2b_I0'][:])
-        result['trueAnomaly'][0]+= sum(J[:3,5]*arg['r_e2b_I0'][:]) + sum(J[3:,5]*arg['v_e2b_I0'][:])
+	def applyDerT(self, arg, result):
+		J = self.J
+		result['altPerigee'][0] += sum(J[:3,0]*arg['r_e2b_I0'][:]) + sum(J[3:,0]*arg['v_e2b_I0'][:])
+		result['altApogee'][0] += sum(J[:3,1]*arg['r_e2b_I0'][:]) + sum(J[3:,1]*arg['v_e2b_I0'][:])
+		result['RAAN'][0] += sum(J[:3,2]*arg['r_e2b_I0'][:]) + sum(J[3:,2]*arg['v_e2b_I0'][:])
+		result['Inc'][0] += sum(J[:3,3]*arg['r_e2b_I0'][:]) + sum(J[3:,3]*arg['v_e2b_I0'][:])
+		result['argPerigee'][0] += sum(J[:3,4]*arg['r_e2b_I0'][:]) + sum(J[3:,4]*arg['v_e2b_I0'][:])
+		result['trueAnomaly'][0]+= sum(J[:3,5]*arg['r_e2b_I0'][:]) + sum(J[3:,5]*arg['v_e2b_I0'][:])
