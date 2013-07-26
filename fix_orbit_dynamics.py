47719f8a3fa4eced72b15f0f4bc0b9e7c77cdc69
Tristan Hearn <tristanhearn@gmail.com>
2013-07-26 15:31:39 -0400

    fixed solar discipline by adding new data files

--- fix_orbit_dynamics.py
+++ fix_orbit_dynamics.py
@@ -1,25 +1,24 @@
-from CADRE.power import Power_CellVoltage
+from CADRE.reactionwheel import ReactionWheel_Dynamics
 from pprint import pprint
 import numpy as np
 import pickle
 import pylab
 
 data = pickle.load(open("data1346.pkl", 'rb'))
-pkloutput = data["0:V_sol"]
+pkloutput = data["0:w_RW"]
 n=1500
-comp = Power_CellVoltage(n)
-comp.time_step = 12*3600. / (n - 1)
+comp = ReactionWheel_Dynamics(n)
+comp.h = 12*3600. / (n - 1)
 
-comp.LOS = data["0:LOS"]
-comp.temperature = data["0:temperature"]
-comp.exposedArea = data["0:exposedArea"]
-comp.Isetpt = data["0:Isetpt"]
+comp.w_B = data["0:w_B"]
+comp.T_RW = data["0:T_RW"]
+#comp.w_RW0 = data["0:w_RW0"]
 comp.run()
 
 print "loaded:",pkloutput
 print 
-print "calc:",comp.V_sol
-print "error:", np.linalg.norm(comp.V_sol - pkloutput) / np.linalg.norm(pkloutput)
+print "calc:",comp.w_RW
+print "error:", np.linalg.norm(comp.w_RW - pkloutput) / np.linalg.norm(pkloutput)
 
 
 
